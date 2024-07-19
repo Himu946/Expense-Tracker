6d8f0716a67a083dfe7ce353d3da4d423e09709b
Himu946 <87778021+Himu946@users.noreply.github.com>
2024-07-20 03:42:15 +0530

    Delete demo.js

--- demo.js
+++
@@ -1,5 +0,0 @@
-const subHeading=document.createElement('h3');
-const text= document.createTextNode('Buy high quality organic fruits online');
-subHeading.appendChild(text);
-subHeading.style.fontWeight="italic";
-
